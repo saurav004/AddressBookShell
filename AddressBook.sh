d49a26f1f8beb53ed102e85276adc382f3b63b0c
saurav004 <logtosaurav@gmail.com>
2020-10-18 22:26:00 +0530

    Modified:made some corrections

--- AddressBook.sh
+++ AddressBook.sh
@@ -1,5 +1,6 @@
 #! /bin/bash/ -x
 shopt -s extglob
+isUpdate=0
 echo "Welcome to Address book code"
 flag=0
 
@@ -7,11 +8,13 @@ flag=0
 
 function openAddressBook()
 {
-end=0
-	while [ $end == 0 ]
-	do
+		end=0
 		echo "Enter Address Book Name"
 		read AddressBookName
+	if [ -f "$AddressBookName.txt" ]
+	then
+		while [ $end == 0 ]
+		do
 		cat "$AddressBookName.txt"
 		echo -e "choose from options given"
 		echo -e "1.\tadd an entry"
@@ -35,11 +38,15 @@ end=0
 			;;
 			5 )
 				end=1
+			;;
 			* )
 				echo "Invalid choice!!"
 			;;
 		esac
 	done
+	else
+			echo "Address Book Not found"
+	fi
 }
 
 #CREATE ADDRESS BOOK
@@ -47,15 +54,24 @@ end=0
 function createAddressBook()
 {
 	end=0
+	yesno=0
 	while [ $end == 0 ]
 	do
 		read -p "Enter Address Book Name" AddressBookName
+		fileName="$AddressBookName.txt"
 		if [ -f "$AddressBookName.txt" ]
 			then
 				echo	"file already exists"
 		else
 			touch "$AddressBookName.txt"
 			echo "AddressBook $AddressBookName created!!"
+		echo -e "First Name\t Last Name\t Address\t City\t State\t Zip\t Phone Number\t" >> $fileName
+		fi
+		echo "want to EXIT 1.yes 2.no"
+		read yesno
+		if [ $yesno == 1 ]
+		then
+		end=1
 		fi
 	done
 }
@@ -98,12 +114,12 @@ function deleteAllAddressBooks(){
 
 function addEntry(){
 fileName="$AddressBookName.txt"
-patFnameLname="^[A-Z]{1}[a-z]{2,}$"
+patFnameLname="^[A-Z]{1}[a-z]{2,30}$"
 patEmail="^[a-zA-Z][a-zA-Z0-9_\-+]*[.]{0,1}[a-zA-Z0-9_\-+]{1,}[@][a-zA-Z0-9]{1,}[.][a-zA-Z]{2,}[.]{0,}[a-zA-Z]*$"
 patMobileNumber="^[+]{1}[0-9]{2}[ ][0-9]{10}"
-patAddress="^[a-zA-Z0-9_\@-+&/]{10,200}$"
-patCity="^[a-zA-Z]{3,50}$"
-patState="^[a-zA-Z]{3,50}"
+patAddress="^[A-Za-z0-9'\.\-\s\,]{10,200}$"
+patCity="^[A-Z]{1}[a-z]{2,50}$"
+patState="^[A-Z]{1}[a-z]{2,50}"
 patZip="^[1-9]{1}[0-9]{2}[ ]{0,1}[0-9]{3}$"
 loop1=0
 loop2=0
@@ -114,10 +130,15 @@ loop6=0
 loop7=0
 while [ $loop1 == 0  ]
 do
+	if [ $isUpdate == 0 ]
+	then
 	read -p "Enter Phone Number" phoneNumber
+	fi
 	if [[ $phoneNumber =~ $patMobileNumber ]]
 	then
 		loop1=1
+	else
+	echo "Innvalid!!"
 	fi
 done
 	searchNumber=`echo $phoneNumber | awk '{print $2}'`
@@ -133,6 +154,8 @@ done
 			if [[ $fName =~ $patFnameLname ]]
     		then
     			loop2=1
+			else
+				echo "Invalid"
     		fi
 		done
 
@@ -142,7 +165,9 @@ done
 			if [[ $lName =~ $patFnameLname ]]
     		then
     			loop3=1
-    		fi
+    		else
+				echo "Invalid"
+			fi
 		done
 
 		while [ $loop4 == 0 ]
@@ -151,7 +176,9 @@ done
 			if [[ $Address =~ $patAddress ]]
     		then
     			loop4=1
-    		fi
+    		else
+				echo "Invalid"
+			fi
 		done
 
    	while [ $loop5 == 0 ]
@@ -160,7 +187,9 @@ done
 			if [[ $city =~ $patCity ]]
     		then
     			loop5=1
-    		fi
+    		else
+				echo "Invalid"
+			fi
 		done
 
 		while [ $loop6 == 0 ]
@@ -169,7 +198,9 @@ done
 			if [[ $state =~ $patState ]]
     		then
     			loop6=1
-    		fi
+    		else
+				echo "Invalid"
+			fi
 		done
 
 		while [ $loop7 == 0 ]
@@ -178,11 +209,14 @@ done
 			if [[ $zip =~ $patZip ]]
    		 then
     			loop7=1
+			else
+				echo "Invalid"
 			fi
 		done
 		echo -e "$fName\t $lName\t $Address\t $city\t $state\t $zip\t $phoneNumber\t" >> $fileName
    	cat $fileName
 	fi
+	isUpdate=0
 }
 
 #DELETE AN ENTRY
@@ -191,30 +225,39 @@ function deleteEntry(){
 
 fileName="$AddressBookName.txt"
 read -p "Enter your phone Number" phoneNumber
-alreadyExists=`cat $fileName | grep $phoneNumber | wc -c`
-    if [ $alreadyExists > 0 ]
+	alreadyExists=0
+   searchNumber=`echo $phoneNumber | awk '{print $2}'`
+   alreadyExists=`cat $fileName | grep $searchNumber | wc -c`
+	 if [ $alreadyExists -gt 0 ]
 	 then
-		cat $fileName | grep -v $phoneNumber > $fileName
+		cat $fileName | grep -v $searchNumber > tmpfile && mv tmpfile $fileName
 		echo "entry deleted"
 		cat $fileName
+	else
+		echo "not found!!"
 	fi
 }
 
 #SEARCH AN ENTRY
 
 function searchEntry(){
-read -p "Enter phone Number" phoneNumber
-ifExists=`cat "$AddressBookName.txt" | grep $phoneNumber | wc -c`
-if [ $ifExists -gt 0 ]
-	then cat "$AddressBookName.txt" | grep $phoneNumber
-	else 
-	echo "Entry doesn't exists"
-fi
+	ifExists=0
+	fileName="$AddressBookName.txt"
+	read -p "Enter phone Number" phoneNumber
+	searchNumber=`echo $phoneNumber | awk '{print $2}'`
+   ifExists=`cat $fileName | grep $searchNumber | wc -c`
+	if [ $ifExists -gt 0 ]
+	then
+		cat $fileName | grep $searchNumber
+	else
+		echo "Entry doesn't exists"
+	fi
 }
 
 #update an Entry
 
 function updateEntry(){
+isUpdate=1
 read -p "Enter phone number of the entry you want to update" phoneNumber
 #echo "what do want to update"
 #echo "choose from below"
@@ -252,16 +295,16 @@ fileName="$AddressBookName.txt"
 #echo "Invalid choice!!"
 #;;
 #esac
-cat $fileName | grep -v $phoneNumber > $fileName
-read -p "Enter First name" fName
-   read -p "Enter Last name" lName
-   read -p "Enter Address" Address
-   read -p "Enter city" city
-   read -p "Enter State" state
-   read -p "Enter zip" zip
-	echo -e "$fName\t $lName\t $Address\t $city\t $state\t $zip\t $phoneNumber\t" >> $fileName
-   cat $fileName
-
+ifExists=0
+searchNumber=`echo $phoneNumber | awk '{print $2}'`
+   ifExists=`cat $fileName | grep $searchNumber | wc -c`
+	if [ $ifExists -gt 0 ]
+	then
+	cat $fileName | grep -v $searchNumber > tmpfile && mv tmpfile $fileName
+	addEntry
+	else
+	echo  "Not found"
+	fi
 }
 
 #Works like Main Menu
@@ -272,14 +315,14 @@ for file in `ls *.txt`
 do
 	((AddressBookNumbering++))
 	AddressBook=`echo $file | awk -F. '{print$1}'`
-	echo "$AddressBookNumbering$AddressBook"
+	echo -e "\t$AddressBookNumbering\t$AddressBook"
 done
-echo "select the options from below"
-echo "1.open an address book"
-echo "2.create an Address book"
-echo "3.delete an Address book"
-echo "4.delete all Address Books"
-echo "5.EXIT"
+echo -e "select the options from below"
+echo -e "\t 1.\topen an address book"
+echo -e "\t 2.\tcreate an Address book"
+echo -e "\t 3.\tdelete an Address book"
+echo -e "\t 4.\tdelete all Address Books"
+echo -e "\t 5.\tEXIT"
 read option
 case $option in
 	1 )
