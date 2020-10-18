d4e62a2af8537da7b8c0b4372c78ec692f9d587a
saurav004 <logtosaurav@gmail.com>
2020-10-18 12:08:25 +0530

    Added:delete deleteAll createAddressBook functions

--- AddressBook.sh
+++ AddressBook.sh
@@ -2,6 +2,8 @@
 
 echo "Welcome to Address book code"
 flag=0
+
+#OPEN ADDRESS BOOK FUNCTION
 function openAddressBook()
 {
 	echo "Enter Address Book Name"
@@ -9,15 +11,58 @@ function openAddressBook()
 	cat "$AddressBookName.txt"
 }
 
+#CREATE ADDRESS BOOK CODE
 function createAddressBook()
 {
 	read -p "Enter Address Book Name" AddressBookName
-	if [ -f $AddressBookName ]
+	if [ -f "$AddressBookName.txt" ]
 		then
-		"file already exists"
-	fi
-	else 
+	echo	"file already exists"
+	else
 	touch "$AddressBookName.txt"
+	fileName="$AddressBookName.txt"
+	echo -e "Fname\t Lname\t Address\t city\t state\t zip\t Phone_Number\t" >> $fileName
+	read -p "Enter First name" fName
+	read -p "Enter Last name" lName
+	read -p "Enter Address" Address
+	read -p "Enter city" city
+	read -p "Enter State" state
+	read -p "Enter zip" zip
+	read -p "Enter Phone Number" phoneNumber
+	echo -e "$fName\t $lName\t $Address\t $city\t $state\t $zip\t $phoneNumber\t" >> $fileName
+	cat "$AddressBookName.txt"
+	fi
+}
+
+#DELETE ADDRESS BOOK CODE
+function deleteAddressBook(){
+flag1=0
+while [ $flag1 == 0 ]
+do
+read -p "enter address book name" AddressBookName
+if [ -f "$AddressBookName.txt" ]
+then
+rm "$AddressBookName.txt"
+echo "$AddressBookName deleted"
+else
+	echo "file doesn't exists input valid name"
+fi
+read -p "wanna exit this window 1.yes 2.no" option1
+if [ $option1 == 1 ]
+then
+flag1=1
+fi
+done
+}
+
+#DELETE ALL ADDRESS BOOK CODE
+
+function deleteAllAddressBooks(){
+
+for file in `ls *.txt`
+do
+rm $file
+done
 }
 
 while [ $flag == 0 ]
@@ -44,7 +89,7 @@ case $option in
 	createAddressBook
 	;;
 	3 )
-	DeleteAddressBook
+	deleteAddressBook
 	;;
 	4 )
 	deleteAllAddressBooks
